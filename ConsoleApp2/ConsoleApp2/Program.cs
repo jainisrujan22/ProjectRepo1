2b73175830d2f14997cd13da0be33a50d048898d
Srujan kumar <Srujankmr92@gmail.com>
2020-03-19 18:14:18 +0530

    Initial commit

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.Write("*");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+
+        }
+    }
+}
